2388ce8e98b732c96a09672b6e18e692c9c61fa7
Phat Tran <ryanz201278@yahoo.com.vn>
2019-03-20 16:25:32 +0700

    Fixed devise configure parameters]

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,10 +23,24 @@ RSpec.describe User, type: :model do
       expect(@user.errors[:email]).to include("can't be blank")
     end
 
+    it 'is invalid without password' do
+      @user.password = nil
+      @user.valid?
+      expect(@user.errors[:password]).to include("can't be blank")
+    end
+
     it 'is invalid if password and confirmation does not match' do
       @user.password_confirmation = "Rm!t2012781357"
       @user.valid?
       expect(@user.errors[:password_confirmation]).to include("doesn't match Password")
+    end
+
+    it 'is invalid if email is duplicated' do
+      @user.save
+      @another_user = User.new(username: "John Cena", email: @user.email,
+                               password: "Rm!t201278", password_confirmation: "Rm!t201278")
+      @another_user.valid?
+      expect(@another_user.errors[:email]).to include("has already been taken")
     end
   end
 end
