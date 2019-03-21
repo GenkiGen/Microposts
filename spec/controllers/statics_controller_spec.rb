3661a4ba8d26946a50ae9eeb743d527f3f533161
Phat Tran <ryanz201278@yahoo.com.vn>
2019-03-21 16:22:07 +0700

    Added static test

--- spec/controllers/statics_controller_spec.rb
+++ spec/controllers/statics_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe StaticsController, type: :controller do
-
+  describe "GET #home" do
+    it "renders home page" do
+      get :home
+      expect(response).to render_template :home
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
+  describe "GET #show" do
+    it "renders user's page"
+  end
 end
