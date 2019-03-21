3881fb550c44a96c55514df9d81e95996622439c
Phat Tran <ryanz201278@yahoo.com.vn>
2019-03-21 12:18:35 +0700

    Restyle user profile page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def format_date(date) 
+    date.strftime("%d/%m/%y")
+  end
+
+  def format_date_time(datetime)
+    datetime.strftime("%k:%M %d/%m/%y")
+  end
+
+  def random_color
+    @colors ||= %w[danger info success warning primary link]
+    @colors[rand(@colors.count)]
+  end
 end
