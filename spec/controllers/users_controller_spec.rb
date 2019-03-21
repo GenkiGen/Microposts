8c77df5e868e114b8e2d8172f1e15cfe3a4b3a07
Phat Tran <ryanz201278@yahoo.com.vn>
2019-03-21 16:25:55 +0700

    Added user controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   describe "GET #show" do
-    it "renders user's page"
+    it "renders user's page" do
+      user = create(:user)
+      get :show, params: { id: user }
+      expect(assigns(:user)).to eq(user)
+    end
   end
 end
